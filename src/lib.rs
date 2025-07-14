6d183653a54b78acb6f040e19f3f4b46e95b9cea
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-07-14 13:07:26 +0200

    Refactoring separating files in modules
    Signed-off-by: Elvis Dukaj <elvis.dukaj@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,4 @@
-use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-use std::net::TcpListener;
+pub mod routes;
+pub mod startup;
 
-#[derive(serde::Deserialize)]
-#[allow(dead_code)]
-struct FormData {
-    name: String,
-    email: String,
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub use startup::*;
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,12 @@
+use actix_web::{HttpResponse, Responder, web};
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    name: String,
+    email: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
