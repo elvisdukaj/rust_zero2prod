91b6f508369fbffe76d1f2e2bbe3d0e35766a43b
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-07-14 10:40:50 +0200

    Implement zerp2prod::run now requires a std::TcpListener
    Improvved also tests so they can bind to random ports and have multiple tests running at the same time

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,25 @@
+use std::fmt::format;
+use std::net::TcpListener;
 use zero2prod;
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Unable to start server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Unable to bind random port");
+    let url = format!("http://127.0.0.1:{}", listener.local_addr().unwrap().port());
+    let server = zero2prod::run(listener).expect("Unable to start server");
     let _ = tokio::spawn(server);
+    url
 }
 
 #[tokio::test]
 async fn test_health_check() {
     // Arrange
-    spawn_app();
+    let url = spawn_app();
 
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(format!("{}/health_check", url))
         .send()
         .await
         .expect("Failed to execute request.");
