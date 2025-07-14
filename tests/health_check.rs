344c28bc2edf4df085653137039f88a98a8f794d
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-07-14 11:03:38 +0200

    Implement /subscribe entrypoint
    Signed-off-by: Elvis Dukaj <elvis.dukaj@gmail.com>

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,17 +12,66 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn test_health_check() {
     // Arrange
-    let url = spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get(format!("{}/health_check", url))
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=my%20user&email=mu%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("email=mu%40gmail.com", "Missing name"),
+        ("name=my%20user", "missing email"),
+        ("", "Missing name and email"),
+    ];
+
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            response.status(),
+            reqwest::StatusCode::BAD_REQUEST,
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
