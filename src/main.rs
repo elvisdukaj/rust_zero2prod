844970303432940796983680069a9b1a76e1de1e
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-07-14 09:27:20 +0200

    Adding actix and tokio with a simple http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+
+async fn greetings(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greetings))
+            .route("/{name}", web::get().to(greetings))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
