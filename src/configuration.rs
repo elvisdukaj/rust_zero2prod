443d43f390237996c718bf8f51bdb717bbbe60ca
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-07-14 13:25:00 +0200

    Adding configuration from file
    Signed-off-by: Elvis Dukaj <elvis.dukaj@gmail.com>

---
+++ src/configuration.rs
@@ -0,0 +1,31 @@
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub port: u16,
+    pub database_name: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ServerSettings {
+    pub host: String,
+    pub port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub server: ServerSettings,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
